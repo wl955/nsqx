c4d35b560ecaf4fbc1f84b772b98b0e9af773a47
jq1 <jq.go@qq.com>
2023-01-17 17:50:01 +0800

    modify opt

--- mq.go
+++ mq.go
@@ -10,62 +10,75 @@ var producer *nsq.Producer
 
 var config = nsq.NewConfig()
 
+var custom = Options{}
+
 func Init(opts ...Option) (e error) {
 	defer func() {
 		if e != nil {
-			if len(consumers) > 0 {
+			if len(custom.lookupdAddr) > 0 {
 				for _, consumer := range consumers {
 					// Gracefully stop the consumer.
 					consumer.Stop()
 				}
 			}
-			if producer != nil {
+			if len(custom.nsqdAddr) > 0 {
 				// Gracefully stop the producer when appropriate (e.g. before shutting down the service)
 				producer.Stop()
 			}
 		}
 	}()
 
-	custom := Options{}
-
 	for _, opt := range opts {
 		opt(&custom)
 	}
 
-	var consumer *nsq.Consumer
+	//有订阅需求
+	if len(custom.lookupdAddr) > 0 {
+		var consumer *nsq.Consumer
 
-	for _, route := range routes {
-		consumer, e = nsq.NewConsumer(route.topic, route.channel, config)
-		if e != nil {
-			return
+		for _, route := range routes {
+			consumer, e = nsq.NewConsumer(route.topic, route.channel, config)
+			if e != nil {
+				return
+			}
+
+			// Set the Handler for messages received by this Consumer. Can be called multiple times.
+			// See also AddConcurrentHandlers.
+			consumer.AddHandler(route.handler)
+
+			// Use nsqlookupd to discover nsqd instances.
+			// See also ConnectToNSQD, ConnectToNSQDs, ConnectToNSQLookupds.
+			e = consumer.ConnectToNSQLookupd(custom.lookupdAddr)
+			if e != nil {
+				return
+			}
+
+			consumers = append(consumers, consumer)
 		}
-
-		// Set the Handler for messages received by this Consumer. Can be called multiple times.
-		// See also AddConcurrentHandlers.
-		consumer.AddHandler(route.handler)
-
-		// Use nsqlookupd to discover nsqd instances.
-		// See also ConnectToNSQD, ConnectToNSQDs, ConnectToNSQLookupds.
-		e = consumer.ConnectToNSQLookupd(custom.lookupdAddr)
-		if e != nil {
-			return
-		}
-
-		consumers = append(consumers, consumer)
 	}
 
-	producer, e = nsq.NewProducer(custom.nsqdAddr, config)
+	//有发布需求
+	if len(custom.nsqdAddr) > 0 {
+		producer, e = nsq.NewProducer(custom.nsqdAddr, config)
+		if e != nil {
+			return
+		}
+	}
 
 	return
 }
 
 func Stop() {
-	for _, consumer := range consumers {
-		// Gracefully stop the consumer.
-		consumer.Stop()
+	if len(custom.lookupdAddr) > 0 {
+		for _, consumer := range consumers {
+			// Gracefully stop the consumer.
+			consumer.Stop()
+		}
 	}
-	// Gracefully stop the producer when appropriate (e.g. before shutting down the service)
-	producer.Stop()
+	if len(custom.nsqdAddr) > 0 {
+		// Gracefully stop the producer when appropriate (e.g. before shutting down the service)
+		producer.Stop()
+	}
 }
 
 func Sub(topic string, channel string, handler nsq.Handler) (e error) {
